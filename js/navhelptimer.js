9b50a0ba427319ff50e94474091c58753472f55c
ThatKARMA <reeper98@mail.ru>
2023-11-13 17:37:28 +0300

    first enter

---
+++ js/navhelptimer.js
@@ -0,0 +1,28 @@
+let sec = 0;
+let hovernav = 0;
+$('#mydivid').css("visibility","visible");
+
+function start() {
+
+    if (hovernav == 0) {
+
+        sec++;
+
+        console.log(sec);
+    
+        if(sec >= 5) {
+            $('#mydivid').css("visibility","hidden");
+            if(sec >= 12) {
+                $('#mydivid').css("visibility","visible");
+                sec = 0;
+                console.log(sec);
+                if(sec = 0) {
+                    start();
+                }
+            }
+        }
+    }
+
+}
+
+let stop = setInterval(start, 1000);
